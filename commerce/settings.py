42b8c1b0fb309997d975f78eade652d3e9abdfcc
Michelangelo <michelangelo@Michelangelo.local>
2021-05-13 18:48:53 +0200

    firstpublic

--- commerce/settings.py
+++ commerce/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','bestbidhouse.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
